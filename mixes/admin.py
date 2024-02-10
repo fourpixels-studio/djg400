04988b4d2c3c9a393031ef701282fcadd41b19fb
Four Pixels <121856163+4ourpixels@users.noreply.github.com>
2024-02-10 22:15:04 +0300

    Second commit 🚀

--- mixes/admin.py
+++ mixes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Album, Genre, Mix, Playlist
+admin.site.register(Album)
+admin.site.register(Genre)
+admin.site.register(Mix)
+admin.site.register(Playlist)
