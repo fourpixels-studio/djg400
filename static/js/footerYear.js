73b098afb7d7db29894f78e8583487b1161ace39
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-07-08 18:41:55 +0300

    Added website link

--- static/js/footerYear.js
+++ static/js/footerYear.js
@@ -1,2 +1,2 @@
 const currentYear = new Date().getFullYear();
-const year = (document.getElementById("footer-year").innerHTML = `&#169; ${currentYear} DJ G400. All rights reserved.`);
+const year = (document.getElementById("footer-year").innerHTML = `&#169; ${currentYear} DJ G400. All rights reserved. <br>Website by <a href="https://fourpixels.studio/" target="_blank">Four Pixels Studio</a>`);
