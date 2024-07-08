5c520a6cd7ad2da158a18c086c373eb0d290e1ea
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-07-08 18:55:31 +0300

    Second commit

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
-
 def shop(request):
-    title_tag = ""
-    meta_descriprion = ""
-    meta_keywords = ""
+    title_tag = "Shop | Dazzling outfits"
+    meta_description = "Shop the latest collection of urban, modern, and fresh merchandise inspired by Arap Trap's unique style."
+    meta_keywords = "400 miles above the competition, arap trap, 4hunnid, g400, highjacked, hood love, trap, hip hop, rnb, rap, quality mixes, hd mixes, 1080p mixes"
     context = {
         'title_tag': title_tag,
+        'meta_description': meta_description,
+        'meta_keywords': meta_keywords,
     }
     return render(request, 'shop.html', context)
