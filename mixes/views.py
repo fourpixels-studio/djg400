01129ef850f7f01148675aeefd9429896ae4561e
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-11 23:15:24 +0300

    Added mixes filters

--- mixes/views.py
+++ mixes/views.py
@@ -11,3 +11,30 @@ def mix_list(request):
     }
     return render(request, 'mix_list.html', context)
 
+
+
+def filtered_albums(request, slug):
+    album = get_object_or_404(Album, slug=slug)
+    context = {
+        'latest_mix': Mix.objects.latest("release_date"),
+        'mixes': Mix.objects.filter(album=album),
+        'all_mixes': Mix.objects.all(),
+        'genres': Genre.objects.all(),
+        'albums': Album.objects.all(),
+        'title_tag': f'Showing all {album} Mixes',
+    }
+    return render(request, 'filtered_mixes.html', context)
+
+
+def filtered_genres(request, slug):
+    genre = get_object_or_404(Genre, slug=slug)
+    context = {
+        'latest_mix': Mix.objects.latest("release_date"),
+        'mixes': Mix.objects.filter(genre=genre),
+        'genres': Genre.objects.all(),
+        'albums': Album.objects.all(),
+        'title_tag': f'Showing all {genre} Mixes',
+    }
+    return render(request, 'filtered_mixes.html', context)
+
+
