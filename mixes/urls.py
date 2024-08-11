761d80b9034655d87b125cb757eeebdac0784c52
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-11 23:16:09 +0300

    Added filtered_albums and filtered_genres

--- mixes/urls.py
+++ mixes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import mix_list
+from .views import mix_list, filtered_genres, filtered_albums
 
 
 urlpatterns = [
     path("", mix_list, name="mix_list"),
+    path("genre/<slug:slug>/", filtered_genres, name="filtered_genres"),
+    path("albums/<slug:slug>/", filtered_albums, name="filtered_albums"),
 ]
