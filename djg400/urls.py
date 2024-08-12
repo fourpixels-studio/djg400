733f199193d9d5a54c56f56073e179f290e5c5a9
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-12 21:49:08 +0300

    Added Hitcount

--- djg400/urls.py
+++ djg400/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path("account/", include("account.urls")),
     path("shop/", include("shop.urls")),
     path("newsletter/", include("newsletter.urls")),
+    path("hitcount/", include("hitcount.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Configure admin titles
