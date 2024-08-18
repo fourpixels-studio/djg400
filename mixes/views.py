89feab0978b5adb9b0e00d5f9b28fca727517c24
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-18 20:07:25 +0300

    Added videos

--- mixes/views.py
+++ mixes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Album, Genre, Mix
 from frontend.utils import update_views
 from seo_management.models import SEO
+from shop.models import Product
 
 
 seo = SEO.objects.first()
@@ -19,6 +20,20 @@ def mix_list(request):
         'genres': Genre.objects.all(),
     }
     return render(request, 'mix_list.html', context)
+
+
+def video_mix_list(request):
+    context = {
+        'title_tag': "Video Mixes",
+        'meta_description': seo.meta_description,
+        'meta_keywords': seo.meta_keywords,
+        'meta_thumbnail': seo.get_thumbnail,
+        'mixes': Mix.objects.order_by("-pk"),
+        'latest_mix': Mix.objects.latest("release_date"),
+        'albums': Album.objects.all(),
+        'genres': Genre.objects.all(),
+    }
+    return render(request, 'video_mix_list.html', context)
 
 
 def filtered_albums(request, slug):
@@ -73,3 +88,23 @@ def mix_detail(request, slug):
     }
     update_views(request, mix)
     return render(request, 'mix_detail.html', context)
+
+
+def video_mix_detail(request, slug):
+    mix = get_object_or_404(Mix, slug=slug)
+    meta_description = f"Volume {mix.episode_number} in the {mix.album.name} series. This mix embodies the spirit of uptown vibes, evoking a world of sophistication, luxury, and refinement in the world of {mix.genre.name} music."
+    meta_keywords = f"{mix.album.name}, {mix.genre.name}, {mix.title}, 400 miles above the competition, arap trap, 4hunnid, g400, highjacked, hood love, trap, hip hop, rnb, rap, quality mixes, hd mixes, 1080p mixes"
+    if mix.meta_thumbnail:
+        meta_thumbnail = mix.meta_thumbnail.url
+    else:
+        meta_thumbnail = None
+    context = {
+        'mix': mix,
+        'title_tag': f"Playing {mix.get_title}",
+        'meta_description': meta_description,
+        'meta_keywords': meta_keywords,
+        'meta_thumbnail': meta_thumbnail,
+        'products': Product.objects.all(),
+    }
+    update_views(request, mix)
+    return render(request, 'video_mix_detail.html', context)
