5a5541ad12b4eef3950cff9f38b1bf69a4ff0278
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-21 20:53:01 +0300

    First commit 🚀

---
+++ mixes/utils.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import Mix
+
+
+def like_mix(request, slug):
+    if request.method == "POST":
+        mix = get_object_or_404(Mix, slug=slug)
+        mix.like_count += 1
+        mix.save()
+        return JsonResponse({"like_count": mix.like_count})
+    return JsonResponse({"error": "Invalid request"}, status=400)
+
+
+def reshare_mix(request, slug):
+    if request.method == "POST":
+        mix = get_object_or_404(Mix, slug=slug)
+        mix.reshare_count += 1
+        mix.save()
+        return JsonResponse({"reshare_count": mix.reshare_count})
+    return JsonResponse({"error": "Invalid request"}, status=400)
