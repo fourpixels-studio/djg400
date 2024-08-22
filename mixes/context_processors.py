9b09b16550dcd4ecd9615c1d7f44b279350cedb4
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-22 15:24:33 +0300

    First commit 🚀

---
+++ mixes/context_processors.py
@@ -0,0 +1,37 @@
+from .models import Mix
+from django.db.models import Q
+
+
+def search_results(request):
+    search_context = {}
+    if request.GET:
+        query = request.GET.get("q")
+        if query:
+            title_results = Q(title__icontains=query)
+            albums_description_results = Q(album__description__icontains=query)
+            genre_results = Q(genre__name__icontains=query)
+            album_name_results = Q(album__name__icontains=query)
+            featured_artists_results = Q(featured_artists__icontains=query)
+            release_date_results = Q(release_date__year=query)
+
+            results = Mix.objects.filter(
+                title_results |
+                genre_results |
+                album_name_results |
+                featured_artists_results |
+                albums_description_results
+            ).order_by('-release_date').distinct()
+
+            if query.isdigit():
+                release_date_results = Q(release_date__year=query)
+                results = results.filter(release_date_results).distinct()
+        else:
+            results = []
+
+        search_context = {
+            "results": results,
+            "query": query,
+            "num_results": results.count(),
+        }
+
+    return search_context
