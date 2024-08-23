0e177d13bb003198b6a3b5f71c3cf7aa5b0396b3
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-23 17:08:20 +0300

    Updated mixes urls

--- mixes/urls.py
+++ mixes/urls.py
@@ -3,15 +3,15 @@ from .views import mix_list, audio_mix_detail, filtered_genres, filtered_albums,
 from .utils import like_mix, reshare_mix
 
 urlpatterns = [
-    path("", mix_list, name="mix_list"),
-    path("<slug:slug>/", audio_mix_detail, name="audio_mix_detail"),
-    path("genre/<slug:slug>/", filtered_genres, name="filtered_genres"),
-    path("albums/<slug:slug>/", filtered_albums, name="filtered_albums"),
-    path("videos/", video_mix_list, name="video_mix_list"),
-    path("video/<slug:slug>/", video_mix_detail, name="video_mix_detail"),
-    path("search/results/", search_mixes, name="search_mixes"),
+    path("mixes/", mix_list, name="mix_list"),
+    path("mix/<slug:slug>/", audio_mix_detail, name="audio_mix_detail"),
+    path("mixes/genre/<slug:slug>/", filtered_genres, name="filtered_genres"),
+    path("mixes/albums/<slug:slug>/", filtered_albums, name="filtered_albums"),
+    path("mixes/videos/", video_mix_list, name="video_mix_list"),
+    path("mixes/video/<slug:slug>/", video_mix_detail, name="video_mix_detail"),
+    path("mixes/search/results/", search_mixes, name="search_mixes"),
 
     # utils
-    path("like-mix/<slug:slug>/", like_mix, name="like_mix"),
-    path("reshare-mix/<slug:slug>/", reshare_mix, name="reshare_mix"),
+    path("mixes/like-mix/<slug:slug>/", like_mix, name="like_mix"),
+    path("mixes/reshare-mix/<slug:slug>/", reshare_mix, name="reshare_mix"),
 ]
