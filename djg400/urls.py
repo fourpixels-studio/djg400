8aa6d252ea99624efdc8d67fd2f61ee2f381b8f4
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-23 17:08:38 +0300

    Updated mixes urls

--- djg400/urls.py
+++ djg400/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("frontend.urls")),
-    path("mixes/", include("mixes.urls")),
+    path("", include("mixes.urls")),
     path("account/", include("account.urls")),
     path("shop/", include("shop.urls")),
     path("newsletter/", include("newsletter.urls")),
