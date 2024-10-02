76de143a6e7ab84b6eeb779a3300eb9844430f07
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-10-02 17:15:04 +0300

    Added form errors to the user

--- account/views.py
+++ account/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm
-from django.core.exceptions import ValidationError
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
 
 
 def account_signin(request):
@@ -24,21 +24,24 @@ def account_signin(request):
                         login(request, user)
                         messages.success(request, f"Welcome back, {user}")
                         return redirect("index")
+                else:
+                    for field, errors in signin_form.errors.items():
+                        for error in errors:
+                            messages.error(request, error)
             except ValidationError as e:
                 messages.error(request, f"Error: {' '.join(e.messages)}")
 
         context.update({
             "signin_form": signin_form,
             "title_tag": "Secure Login",
-            "meta_description": "Log in to your Sibia account to access personalized healthcare solutions, AI-driven insights, and SDG research for improving community health.",
-            "meta_keywords": "Sibia, login, secure access, healthcare account, medical AI, personalized healthcare, community health, SDG research",
+            "meta_keywords": "DJ G400, Sign In, Log In",
+            "meta_description": "Log in to access exclusive releases, latest mixes, ad-free experienec and curated playlists.",
         })
         return render(request, "account_signin.html", context)
 
 
 @login_required
 def account_signout(request):
-    print(f"{request.user} logged out!")
     logout(request)
     return redirect("account_signin")
 
@@ -46,32 +49,38 @@ def account_signout(request):
 def account_signup(request):
     if request.user.is_authenticated:
         return redirect("index")
-    else:
-        context = {}
-        if request.method == "POST":
-            register_user_form = CustomUserCreationForm(request.POST)
+
+    context = {}
+
+    if request.method == "POST":
+        register_user_form = CustomUserCreationForm(request.POST)
+        try:
             if register_user_form.is_valid():
                 user = register_user_form.save()
                 username = register_user_form.cleaned_data.get('username')
-                raw_password = register_user_form.cleaned_data.get('password1')
-                user = authenticate(username=username, password=raw_password)
+                password = register_user_form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=password)
                 if user is not None:
                     login(request, user)
-                    messages.success(
-                        request, f"Welcome, {username}! You have successfully signed up.")
+                    messages.success(request, f"Welcome, {username}! You have successfully signed up.")
                     return redirect("index")
                 else:
                     messages.error(
                         request, "There was an error with authentication.")
             else:
-                messages.error(request, "There was an error with the form.")
-        else:
-            register_user_form = CustomUserCreationForm()
+                for field, errors in register_user_form.errors.items():
+                    for error in errors:
+                        messages.error(request, error)
+        except ValidationError as e:
+            messages.error(request, f"Error: {', '.join(e.messages)}")
+    else:
+        register_user_form = CustomUserCreationForm()
 
-        context.update({
-            "register_user_form": register_user_form,
-            "title_tag": "Create Account",
-            "meta_description": "Create your Sibia account to access personalized healthcare solutions, AI-driven insights, and SDG research for improving community health.",
-            "meta_keywords": "Sibia, signin, secure access, healthcare account, medical AI, personalized healthcare, community health, SDG research",
-        })
-        return render(request, "account_signup.html", context)
+    context.update({
+        "title_tag": "Create Account",
+        "register_user_form": register_user_form,
+        "meta_keywords": "DJ G400, Sign Up, Create Account",
+        "meta_description": "Create your account to access exclusive releases, latest mixes, ad-free experience, and curated playlists.",
+    })
+
+    return render(request, "account_signup.html", context)
