03ef10253aa82b2584cf7e6560a7e106e26e55b6
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-11-03 15:46:10 +0300

    Added recaptcha

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,8 +1,18 @@
-from .models import Newsletter
 from django import forms
+from .models import Newsletter
 from django.forms import ModelForm
+from django_recaptcha.fields import ReCaptchaField
+
 
 class NewsletterForm(forms.ModelForm):
+    captcha = ReCaptchaField()
+
     class Meta:
         model = Newsletter
-        fields = ('email',)
+        fields = ('email', 'captcha')
+
+    def clean_captcha(self):
+        captcha_value = self.cleaned_data.get('captcha')
+        if not captcha_value:
+            raise forms.ValidationError("Please complete the captcha.")
+        return captcha_value
