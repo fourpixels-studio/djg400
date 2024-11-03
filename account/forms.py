6ab4c09fa274274cf78219bccfc45a37abcb0a37
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-11-03 15:45:22 +0300

    Added recaptcha

--- account/forms.py
+++ account/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
+from django_recaptcha.fields import ReCaptchaField
 from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomUserCreationForm(UserCreationForm):
+    captcha = ReCaptchaField()
     email = forms.EmailField(required=True)
 
     class Meta:
@@ -13,7 +15,8 @@ class CustomUserCreationForm(UserCreationForm):
             'email',
             'first_name',
             'password1',
-            'password2'
+            'password2',
+            'captcha',
         )
 
     def save(self, commit=True):
@@ -22,3 +25,9 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+        
+    def clean_captcha(self):
+        captcha_value = self.cleaned_data.get('captcha')
+        if not captcha_value:
+            raise forms.ValidationError("Please complete the captcha.")
+        return captcha_value
