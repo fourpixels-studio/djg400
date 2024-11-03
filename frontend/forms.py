04641aa2b912eca1078853d504b54200fa8915a6
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-11-03 15:34:35 +0300

    First commit 🚀

---
+++ frontend/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Contact
+from django_recaptcha.fields import ReCaptchaField
+
+
+class ContactForm(forms.ModelForm):
+    captcha = ReCaptchaField()
+
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'message', 'captcha']
+
+    def clean_captcha(self):
+        captcha_value = self.cleaned_data.get('captcha')
+        if not captcha_value:
+            raise forms.ValidationError("Please complete the captcha.")
+        return captcha_value
