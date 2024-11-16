8df9c34e9223e1ec9be7346e6de6aa5a19db8f2d
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-11-16 20:07:07 +0300

    Added shop, payment. cart and orders urls

--- djg400/urls.py
+++ djg400/urls.py
@@ -21,16 +21,21 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path("", include("frontend.urls")),
     path("", include("mixes.urls")),
     path("account/", include("account.urls")),
     path("shop/", include("shop.urls")),
     path("newsletter/", include("newsletter.urls")),
+    path("playlists/", include("playlists.urls")),
     path("hitcount/", include("hitcount.urls")),
-    path("remixes/", include("remixes.urls")),
     path("summernote/", include('django_summernote.urls')),
+    path("remixes/", include("remixes.urls")),
     path("blogs/", include("blogs.urls")),
+    path("orders/", include('orders.urls')),
+    path("shop/", include("products.urls")),
+    path("cart/", include("cart.urls")),
+    path("", include("payments.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Configure admin titles
