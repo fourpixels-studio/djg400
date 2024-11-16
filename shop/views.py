ffdebc67d61a1f699f7158dba8d64826cd049cca
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-11-16 20:00:02 +0300

    Removed view_product and updated the shop SEO

--- shop/views.py
+++ shop/views.py
@@ -1,29 +1,14 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render
+from products.models import Product
+from seo_management.models import SEO
 
 
 def shop(request):
-    title_tag = "Shop | Dazzling outfits"
-    meta_description = "Shop the latest collection of urban, modern, and fresh merchandise inspired by Arap Trap's unique style."
-    meta_keywords = "400 miles above the competition, arap trap, 4hunnid, g400, highjacked, hood love, trap, hip hop, rnb, rap, quality mixes, hd mixes, 1080p mixes"
+    seo = SEO.objects.get(pk=4)
     context = {
-        'title_tag': title_tag,
-        'meta_description': meta_description,
-        'meta_keywords': meta_keywords,
+        'title_tag': seo.title_tag,
         'products': Product.objects.all(),
-        'latest_product': Product.objects.first(),
+        'meta_keywords': seo.meta_keywords,
+        'meta_description': seo.meta_description,
     }
     return render(request, 'shop.html', context)
-
-
-def view_product(request, slug):
-    product = get_object_or_404(Product, slug=slug)
-    meta_description = "Shop the latest collection of urban, modern, and fresh merchandise inspired by Arap Trap's unique style."
-    meta_keywords = "400 miles above the competition, arap trap, 4hunnid, g400, highjacked, hood love, trap, hip hop, rnb, rap, quality mixes, hd mixes, 1080p mixes"
-    context = {
-        'title_tag': product.name,
-        'meta_description': meta_description,
-        'meta_keywords': meta_keywords,
-        'product': product,
-    }
-    return render(request, 'view_product.html', context)
