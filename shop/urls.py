26f84c1e4a81be3d5217b0fce47dd52c997c76b6
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-11-16 20:00:31 +0300

    Removed view_product

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,7 @@
+from .views import shop
 from django.urls import path
-from .views import shop, view_product
 
 
 urlpatterns = [
     path("", shop, name="shop"),
-    path("<slug:slug>/", view_product, name="view_product"),
 ]
