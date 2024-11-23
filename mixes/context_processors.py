d7cfe50c67720e4bc28bff58a9dfdeb2b7d4a9b7
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-11-23 22:07:24 +0300

    Added Blogs and Paginator

--- mixes/context_processors.py
+++ mixes/context_processors.py
@@ -1,13 +1,17 @@
 from .models import Mix
+from blogs.models import Blog
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def search_results(request):
     search_context = {}
-    random_mixes = Mix.objects.order_by('?')[:10]
+    random_mixes = Mix.objects.order_by('?')[:3]
+
     if request.GET:
         query = request.GET.get("q")
         if query:
+            # Mix model search criteria
             title_results = Q(title__icontains=query)
             albums_description_results = Q(album__description__icontains=query)
             genre_results = Q(genre__name__icontains=query)
@@ -15,7 +19,7 @@ def search_results(request):
             featured_artists_results = Q(featured_artists__icontains=query)
             release_date_results = Q(release_date__year=query)
 
-            results = Mix.objects.filter(
+            mix_results = Mix.objects.filter(
                 title_results |
                 genre_results |
                 album_name_results |
@@ -25,14 +29,58 @@ def search_results(request):
 
             if query.isdigit():
                 release_date_results = Q(release_date__year=query)
-                results = results.filter(release_date_results).distinct()
+                mix_results = mix_results.filter(
+                    release_date_results).distinct()
+
+            # Blog model search criteria
+            blog_title_results = Q(title__icontains=query)
+            blog_summary_results = Q(summary__icontains=query)
+            blog_content_results = Q(content__icontains=query)
+            blog_keywords_results = Q(keywords__icontains=query)
+            blog_category_results = Q(category__icontains=query)
+
+            blog_results = Blog.objects.filter(
+                blog_title_results |
+                blog_summary_results |
+                blog_content_results |
+                blog_keywords_results |
+                blog_category_results
+            ).filter(is_published=True).order_by('-published_date').distinct()
+
+            mixes_paginator = Paginator(mix_results, 9)
+            page = request.GET.get("page", 1)
+
+            try:
+                mixes = mixes_paginator.page(page)
+            except (PageNotAnInteger, EmptyPage):
+                mixes = mixes_paginator.page(1)
+
+            blogs_paginator = Paginator(blog_results, 9)
+            page = request.GET.get("page", 1)
+
+            try:
+                blogs = blogs_paginator.page(page)
+            except (PageNotAnInteger, EmptyPage):
+                blogs = blogs_paginator.page(1)
+
+            results = {
+                "mixes": mixes,
+                "blogs": blogs,
+            }
+
         else:
-            results = []
+            results = {
+                "mixes": [],
+                "blogs": [],
+            }
 
         search_context = {
             "results": results,
             "query": query,
-            "num_results": results.count(),
+            "num_results": {
+                "mixes": len(results["mixes"]),
+                "blogs": len(results["blogs"]),
+            },
             'random_mixes': random_mixes,
         }
 
