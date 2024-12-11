ce80e65c971120e15e6820f940d6bc69c2aa0327
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-11 22:21:42 +0300

    Added unsubscribe_newsletter and updated the subscription

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import newsletter
+from .views import subscribe_newsletter, unsubscribe_newsletter
 
 
 urlpatterns = [
-    path("", newsletter, name="newsletter"),
+    path("subscribe/", subscribe_newsletter, name="subscribe_newsletter"),
+    path("unsubscribe/", unsubscribe_newsletter, name="unsubscribe_newsletter"),
 ]
