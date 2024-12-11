faaba39252d092161e0cb52e2b3c6755928f044f
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-11 22:22:20 +0300

    Added unsubscription form

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -16,3 +16,24 @@ class NewsletterForm(forms.ModelForm):
         if not captcha_value:
             raise forms.ValidationError("Please complete the captcha.")
         return captcha_value
+
+
+class UnsubscribeForm(forms.Form):
+    captcha = ReCaptchaField()
+    email = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter your email'})
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        captcha_value = cleaned_data.get('captcha')
+        if not captcha_value:
+            self.add_error('captcha', "Please complete the captcha.")
+        return cleaned_data
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['captcha'].required = True
