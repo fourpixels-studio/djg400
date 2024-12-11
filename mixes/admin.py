550c31085010debbfcd625eff611bf720ec58ade
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-11 19:13:01 +0300

    Moved Playlist to playlists app

--- mixes/admin.py
+++ mixes/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Album, Genre, Mix, Playlist
+from .models import Album, Genre, Mix
+admin.site.register(Mix)
 admin.site.register(Album)
 admin.site.register(Genre)
-admin.site.register(Mix)
-admin.site.register(Playlist)
