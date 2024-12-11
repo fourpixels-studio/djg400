bbad1464942538402fe76b93297554863f4dd693
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-11 19:31:49 +0300

    Changed the playlist order to release_date

--- playlists/views.py
+++ playlists/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 def playlists(request):
     context = {
         "title_tag": "Playlists",
-        "playlists": Playlist.objects.all(),
+        "playlists": Playlist.objects.order_by("-release_date"),
     }
     return render(request, 'playlists.html', context)
