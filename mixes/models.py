2be7398a004edea54322200a70784d8010b68193
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-17 19:47:01 +0300

    Added get_share_link and the imported the settings

--- mixes/models.py
+++ mixes/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.utils.text import slugify
 from django.urls import reverse
+from django.conf import settings
+from hitcount.models import HitCount
+from django.utils.text import slugify
 from django_resized import ResizedImageField
 from django.templatetags.static import static
-from hitcount.models import HitCount
 from django.contrib.contenttypes.fields import GenericRelation
 
 
@@ -12,10 +13,8 @@ class Album(models.Model):
     slug = models.SlugField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     image = models.TextField(null=True, blank=True)
-    square_cover = models.ImageField(
-        upload_to="mixes/covers/", blank=True, null=True)
-    landscape_cover = models.ImageField(
-        upload_to="mixes/covers/", blank=True, null=True)
+    square_cover = models.ImageField(upload_to="mixes/covers/", blank=True, null=True)
+    landscape_cover = models.ImageField(upload_to="mixes/covers/", blank=True, null=True)
     meta_thumbnail = ResizedImageField(
         size=[1200, 630],
         crop=['middle', 'center'],
@@ -25,7 +24,7 @@ class Album(models.Model):
         null=True
     )
     square_thumbnail = ResizedImageField(
-        size=[1200, 1200],
+        size=[100, 100],
         crop=['middle', 'center'],
         quality=75,
         upload_to='mixes/thumbnails/',
@@ -43,7 +42,12 @@ class Album(models.Model):
         return reverse("filtered_albums", kwargs={
             "slug": self.slug,
         })
-
+        
+    @property
+    def get_hit_count(self):
+        if self.hit_count_generic.exists():
+            return self.hit_count_generic.first().hits
+        return 0
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
@@ -65,10 +69,8 @@ class Genre(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     slug = models.SlugField(null=True, blank=True)
     image = models.TextField(null=True, blank=True)
-    square_cover = models.ImageField(
-        upload_to="mixes/covers/", blank=True, null=True)
-    landscape_cover = models.ImageField(
-        upload_to="mixes/covers/", blank=True, null=True)
+    square_cover = models.ImageField(upload_to="mixes/covers/", blank=True, null=True)
+    landscape_cover = models.ImageField(upload_to="mixes/covers/", blank=True, null=True)
     meta_thumbnail = ResizedImageField(
         size=[1200, 630],
         crop=['middle', 'center'],
@@ -78,15 +80,14 @@ class Genre(models.Model):
         null=True
     )
     square_thumbnail = ResizedImageField(
-        size=[1200, 1200],
+        size=[100, 100],
         crop=['middle', 'center'],
         quality=75,
         upload_to='mixes/thumbnails/',
         blank=True,
         null=True
     )
-    hit_count_generic = GenericRelation(
-        HitCount, object_id_field='object_pk', related_query_name='hit_count_generic_relation')
+    hit_count_generic = GenericRelation(HitCount, object_id_field='object_pk', related_query_name='hit_count_generic_relation')
 
     def __str__(self):
         return self.name
@@ -96,6 +97,12 @@ class Genre(models.Model):
         return reverse("filtered_genres", kwargs={
             "slug": self.slug,
         })
+        
+    @property
+    def get_hit_count(self):
+        if self.hit_count_generic.exists():
+            return self.hit_count_generic.first().hits
+        return 0
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -116,10 +123,8 @@ class Genre(models.Model):
 
 class Mix(models.Model):
     title = models.CharField(max_length=150, blank=True, null=True)
-    square_cover = models.ImageField(
-        upload_to="mixes/covers/", blank=True, null=True)
-    landscape_cover = models.ImageField(
-        upload_to="mixes/covers/", blank=True, null=True)
+    square_cover = models.ImageField(upload_to="mixes/covers/", blank=True, null=True)
+    landscape_cover = models.ImageField(upload_to="mixes/covers/", blank=True, null=True)
     meta_thumbnail = ResizedImageField(
         size=[1200, 630],
         crop=['middle', 'center'],
@@ -129,7 +134,7 @@ class Mix(models.Model):
         null=True
     )
     square_thumbnail = ResizedImageField(
-        size=[1200, 1200],
+        size=[100, 100],
         crop=['middle', 'center'],
         quality=75,
         upload_to='mixes/thumbnails/',
@@ -138,33 +143,23 @@ class Mix(models.Model):
     )
     episode_number = models.CharField(max_length=3, blank=True, null=True)
     color = models.CharField(max_length=15, blank=True, null=True)
-    album = models.ForeignKey(
-        Album, on_delete=models.CASCADE, blank=True, null=True)
-    genre = models.ForeignKey(
-        Genre, on_delete=models.CASCADE, blank=True, null=True)
-    stream_link = models.TextField(blank=True, null=True)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, blank=True, null=True)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, blank=True, null=True)
     youtube_link = models.TextField(blank=True, null=True)
+    stream_link = models.TextField(blank=True, null=True)
     is_popular = models.BooleanField(default=False, blank=True, null=True)
     featured_artists = models.TextField(blank=True, null=True)
     similar_mixes = models.CharField(max_length=100, blank=True, null=True)
     release_date = models.DateField(blank=True, null=True)
     play_count = models.PositiveIntegerField(default=0, blank=True, null=True)
-    download_count = models.PositiveIntegerField(
-        default=0, blank=True, null=True)
-    reshare_count = models.PositiveIntegerField(
-        default=0, blank=True, null=True)
+    download_count = models.PositiveIntegerField(default=0, blank=True, null=True)
+    reshare_count = models.PositiveIntegerField(default=0, blank=True, null=True)
     like_count = models.PositiveIntegerField(default=0, blank=True, null=True)
     slug = models.SlugField(unique=True, null=True, blank=True)
     video = models.FileField(null=True, blank=True)
-    hit_count_generic = GenericRelation(
-        HitCount, object_id_field='object_pk', related_query_name='hit_count_generic_relation')
+    hit_count_generic = GenericRelation(HitCount, object_id_field='object_pk', related_query_name='hit_count_generic_relation')
 
     def __str__(self):
-        if self.get_year:
-            color = self.color
-        else:
-            color = None
-
         return f"{self.pk} - {self.title}"
 
     @property
@@ -186,15 +181,16 @@ class Mix(models.Model):
 
     @property
     def get_url(self):
-        return reverse("mix_detail", kwargs={
+        return reverse("audio_mix_detail", kwargs={
             "slug": self.slug,
         })
-
+        
     @property
     def get_video_url(self):
         return reverse("video_mix_detail", kwargs={
             "slug": self.slug,
         })
+
 
     @property
     def get_square_cover(self):
@@ -220,12 +216,6 @@ class Mix(models.Model):
             return self.landscape_cover.url
         return static('landscape_cover.png')
 
-    @property
-    def get_landscape_thumbnail(self):
-        if self.meta_thumbnail:
-            return self.meta_thumbnail.url
-        return static('landscape_cover.png')
-
     # @property
     # def get_similar_mixes(self):
     #     if self.similar_mixes:
@@ -248,7 +238,13 @@ class Mix(models.Model):
         if self.release_date:
             return self.release_date.year
         return None
-
+        
+    @property
+    def get_landscape_thumbnail(self):
+        if self.meta_thumbnail:
+            return self.meta_thumbnail.url
+        return static('landscape_cover.png')
+        
     @property
     def get_hit_count(self):
         if self.hit_count_generic.exists():
@@ -270,3 +266,9 @@ class Mix(models.Model):
             self.square_cover.save(
                 f"{self.square_cover.name}", self.square_cover, save=False)
             super().save(update_fields=['meta_thumbnail'])
+            
+    @property
+    def get_share_link(self):
+        current_site = settings.PUBLIC_URL
+        blog_url = self.get_url
+        return f"{current_site}{blog_url}"
