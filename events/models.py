7059d4fb7df0007cadc697f8b271df0c53c5117c
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-21 00:07:10 +0300

    Updated the EventMedia

--- events/models.py
+++ events/models.py
@@ -10,8 +10,7 @@ from django.contrib.contenttypes.fields import GenericRelation
 
 class Event(models.Model):
     name = models.CharField(max_length=255)
-    poster = models.ImageField(
-        upload_to='event_posters/', null=True, blank=True)
+    poster = models.ImageField(upload_to='event_posters/', null=True, blank=True)
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
@@ -19,13 +18,10 @@ class Event(models.Model):
     venue = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     ticket_link = models.TextField(blank=True, null=True)
-    poster = models.ImageField(
-        upload_to="event_posters/", blank=True, null=True)
+    poster = models.ImageField(upload_to="event_posters/", blank=True, null=True)
     keywords = models.TextField(blank=True, null=True)
-    meta_thumbnail = ResizedImageField(
-        size=[1200, 600], quality=75, upload_to='thumbnails/', blank=True, null=True)
-    hit_count_generic = GenericRelation(
-        HitCount, object_id_field='object_pk', related_query_name='hit_count_generic_relation')
+    meta_thumbnail = ResizedImageField(size=[1200, 600], quality=75, upload_to='thumbnails/', blank=True, null=True)
+    hit_count_generic = GenericRelation(HitCount, object_id_field='object_pk', related_query_name='hit_count_generic_relation')
     slug = models.SlugField(unique=True, null=True, blank=True)
     portrait_thumbnail = ResizedImageField(size=[540, 675], crop=[
                                            'middle', 'center'], quality=75, upload_to='events/thumbnails/', blank=True, null=True)
@@ -76,24 +72,12 @@ class Event(models.Model):
 
 
 class EventMedia(models.Model):
-    event = models.ForeignKey(
-        Event, on_delete=models.SET_NULL, null=True, blank=True)
+    event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='event_media/', null=True, blank=True)
     youtube_link = models.TextField(null=True, blank=True)
     youtube_embed_link = models.TextField(null=True, blank=True)
-    video_file = models.FileField(
-        null=True, blank=True, upload_to="event_media/")
+    video_file = models.FileField(null=True, blank=True, upload_to="event_media/")
 
     def __str__(self):
-        if self.image:
-            file = str("IMAGE")
-        elif self.youtube_link:
-            file = str("YOUTUBE LINK")
-        elif self.youtube_embed_link:
-            file = str("YOUTUBE EMBED LINK")
-        elif self.video_file:
-            file = str("VIDEO FILE")
-        else:
-            file = "NO FILE"
-        return f"{file} --- {self.name} --- {self.event.pk}"
+        return f"{self.name} --- {self.event}"
