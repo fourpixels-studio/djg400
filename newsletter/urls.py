8d9a3eb4ca710ec9b5e5e162ad664c7397ba8154
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-23 20:19:44 +0300

    Added send newsletters urls

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import subscribe_newsletter, unsubscribe_newsletter
+from .views import subscribe_newsletter, unsubscribe_newsletter, send_newsletter_article, newsletter_detail
 
 
 urlpatterns = [
+    path("sent/<int:pk>/", newsletter_detail, name="newsletter_detail"),
     path("subscribe/", subscribe_newsletter, name="subscribe_newsletter"),
+    path("send/", send_newsletter_article, name="send_newsletter_article"),
     path("unsubscribe/", unsubscribe_newsletter, name="unsubscribe_newsletter"),
 ]
