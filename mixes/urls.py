940ba1d17742ed599b54e9cc73537429cfb6769f
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-24 21:30:57 +0300

    Moved search to frontend

--- mixes/urls.py
+++ mixes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import mix_list, audio_mix_detail, filtered_genres, filtered_albums, video_mix_list, video_mix_detail, search_mixes
+from .views import mix_list, audio_mix_detail, filtered_genres, filtered_albums, video_mix_list, video_mix_detail
 from .utils import like_mix, reshare_mix
 
 urlpatterns = [
@@ -9,7 +9,6 @@ urlpatterns = [
     path("mixes/albums/<slug:slug>/", filtered_albums, name="filtered_albums"),
     path("mixes/videos/", video_mix_list, name="video_mix_list"),
     path("mixes/video/<slug:slug>/", video_mix_detail, name="video_mix_detail"),
-    path("mixes/search/results/", search_mixes, name="search_mixes"),
 
     # utils
     path("mixes/like-mix/<slug:slug>/", like_mix, name="like_mix"),
