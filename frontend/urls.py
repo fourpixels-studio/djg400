0e4936b57ecf02c6a621f501505c8d4fa31421d0
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-24 21:19:53 +0300

    Added search

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, about
+from .views import index, about, search
 
 
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
+    path("search/results/", search, name="search"),
 ]
