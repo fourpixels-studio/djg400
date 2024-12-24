dca9b687f577f70da25a5ac50e78ee204532f56e
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-24 21:30:12 +0300

    Changed the search function to come from frontend

--- djg400/settings.py
+++ djg400/settings.py
@@ -82,7 +82,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'mixes.context_processors.search_results',
+                'frontend.context_processors.search_results',
             ],
         },
     },
