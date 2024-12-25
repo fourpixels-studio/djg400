f217a6958cac1e351ed0db2bc18d301007664ad1
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-25 21:51:14 +0300

    Added playlist_detail

--- playlists/urls.py
+++ playlists/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import playlists
+from .views import playlists, playlist_detail
 
 
 urlpatterns = [
     path("", playlists, name="playlists"),
+    path("<slug:slug>/", playlist_detail, name="playlist_detail"),
 ]
