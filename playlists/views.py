f84001a57994de2b6f648695257f4e66d7b12cc6
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-25 21:50:54 +0300

    Added SEO and playlist_detail view

--- playlists/views.py
+++ playlists/views.py
@@ -1,10 +1,27 @@
 from .models import Playlist
 from django.shortcuts import render
+from seo_management.models import SEO
 
 
 def playlists(request):
+    seo = SEO.objects.get(pk=9)
     context = {
-        "title_tag": "Playlists",
+        "title_tag": seo.title_tag,
+        "meta_keywords": seo.meta_keywords,
+        "meta_thumbnail": seo.get_thumbnail,
+        "meta_description": seo.meta_description,
         "playlists": Playlist.objects.order_by("-release_date"),
     }
     return render(request, 'playlists.html', context)
+
+
+def playlist_detail(request, slug):
+    playlist = Playlist.objects.get(slug=slug)
+    context = {
+        "playlist": playlist,
+        "meta_thumbnail": playlist.get_thumbnail,
+        "title_tag": f"Playlist | {playlist.title} Hits",
+        "meta_description": f"Immerse yourself in {playlist.title}, a playlist by DJ G400 featuring the best of {playlist.genre.name}. Available on Spotify and YouTube, download your favorite tracks and videos.",
+        "meta_keywords": f"DJ G400 playlist, {playlist.genre.name} playlist, Spotify playlist, YouTube playlist, curated {playlist.genre.name} music, DJ G400 Spotify, DJ G400 YouTube, music downloads, trending playlists",
+    }
+    return render(request, 'playlist_detail.html', context)
