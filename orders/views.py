87ae17abb701c966a7114f76f9023f06598d01b5
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-26 20:25:16 +0300

    Removed spaces

--- orders/views.py
+++ orders/views.py
@@ -11,22 +11,16 @@ def view_order(request, order_number):
         order = Order.objects.get(order_number=order_number)
         if order.paid == True and order.is_email_sent == False:
             pdf_output = generate_receipt(order)
-            order.receipt.save(
-                f'order_{order.get_order_number}.pdf',
-                ContentFile(pdf_output), save=True
-            )
+            order.receipt.save(f'order_{order.get_order_number}.pdf', ContentFile(pdf_output), save=True)
             order.save()
             send_order_confirmation_email(order)
             order.is_email_sent = True
             order.save()
-            messages.success(
-                request, "Thank you for your order! A confirmation email with your order details and receipt will be sent to you shortly.")
-
+            messages.success(request, "Thank you for your order! A confirmation email with your order details and receipt will be sent to you shortly.")
         context = {
             "order": order,
             "title_tag": f"Order No. #{order.get_order_number} Confirmed!",
         }
         return render(request, 'view_order.html', context)
-
     except Order.DoesNotExist:
         return redirect('shop')
