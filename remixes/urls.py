76f5c9f244033131d287dc200ae6cb4f1e77d7d3
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-26 17:46:59 +0300

    Added support_remix url

--- remixes/urls.py
+++ remixes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import remixes_list, remix_detail
+from .views import remixes_list, remix_detail, support_remix
 
 urlpatterns = [
     path("", remixes_list, name="remixes_list"),
+    path("support/", support_remix, name="support_remix"),
     path("remix/<slug:slug>/", remix_detail, name="remix_detail"),
 ]
