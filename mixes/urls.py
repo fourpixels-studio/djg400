51ff956e004b839a960fc158b667bd94750c6b9a
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-28 15:50:07 +0300

    Added support_mix

--- mixes/urls.py
+++ mixes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .utils import like_mix, reshare_mix
-from .views import mix_list, filtered_genres, filtered_albums, mix_detail
+from .views import mix_list, filtered_genres, filtered_albums, mix_detail, support_mix
 
 urlpatterns = [
     path("mixes/", mix_list, name="mix_list"),
+    path("support/mix/", support_mix, name="support_mix"),
     path("mix/<slug:slug>/", mix_detail, name="mix_detail"),
     path("mixes/genre/<slug:slug>/", filtered_genres, name="filtered_genres"),
     path("mixes/albums/<slug:slug>/", filtered_albums, name="filtered_albums"),
