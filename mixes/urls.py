dfb6e65b9935f97181d8c87ecadc4f6159e4f5f6
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-28 15:12:29 +0300

    Removed video and audio mix detail

--- mixes/urls.py
+++ mixes/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from .views import mix_list, audio_mix_detail, filtered_genres, filtered_albums, video_mix_list, video_mix_detail
 from .utils import like_mix, reshare_mix
+from .views import mix_list, filtered_genres, filtered_albums, mix_detail
 
 urlpatterns = [
     path("mixes/", mix_list, name="mix_list"),
-    path("mix/<slug:slug>/", audio_mix_detail, name="audio_mix_detail"),
+    path("mix/<slug:slug>/", mix_detail, name="mix_detail"),
     path("mixes/genre/<slug:slug>/", filtered_genres, name="filtered_genres"),
     path("mixes/albums/<slug:slug>/", filtered_albums, name="filtered_albums"),
-    path("mixes/videos/", video_mix_list, name="video_mix_list"),
-    path("mixes/video/<slug:slug>/", video_mix_detail, name="video_mix_detail"),
 
     # utils
     path("mixes/like-mix/<slug:slug>/", like_mix, name="like_mix"),
