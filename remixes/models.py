ce7569028c8cf64bdcb96e11ac28ec59ef84e979
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-30 20:21:00 +0300

    Updated the url fields

--- remixes/models.py
+++ remixes/models.py
@@ -1,11 +1,9 @@
-import os
 from django.db import models
 from django.urls import reverse
 from colorthief import ColorThief
 from hitcount.models import HitCount
 from django.utils.text import slugify
 from django_resized import ResizedImageField
-from django.templatetags.static import static
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.contrib.contenttypes.fields import GenericRelation
@@ -35,12 +33,10 @@ class RemixGenre(models.Model):
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
         if self.landscape_cover and (not self.meta_thumbnail or self.meta_thumbnail.name != f"{self.landscape_cover.name}"):
-            self.landscape_cover.save(
-                f"{self.landscape_cover.name}", self.landscape_cover, save=False)
+            self.landscape_cover.save(f"{self.landscape_cover.name}", self.landscape_cover, save=False)
             super().save(update_fields=['meta_thumbnail'])
         if self.square_cover and (not self.meta_thumbnail or self.meta_thumbnail.name != f"{self.square_cover.name}"):
-            self.square_cover.save(
-                f"{self.square_cover.name}", self.square_cover, save=False)
+            self.square_cover.save(f"{self.square_cover.name}", self.square_cover, save=False)
             super().save(update_fields=['meta_thumbnail'])
 
 
@@ -53,8 +49,8 @@ class Remix(models.Model):
     square_thumbnail = ResizedImageField(size=[150, 150], crop=['middle', 'center'], quality=75, upload_to='remixes/thumbnails/', blank=True, null=True)
     genre = models.ForeignKey(RemixGenre, on_delete=models.CASCADE, blank=True, null=True)
     is_popular = models.BooleanField(default=False, blank=True, null=True)
-    video_download_link = models.TextField(blank=True, null=True)
-    audio_download_link = models.TextField(blank=True, null=True)
+    video_download_link = models.URLField(blank=True, null=True)
+    audio_download_link = models.URLField(blank=True, null=True)
     similar_remixes = models.CharField(max_length=100, blank=True, null=True)
     release_date = models.DateField(blank=True, null=True)
     video_download_count = models.PositiveIntegerField(default=0, blank=True, null=True)
@@ -76,8 +72,7 @@ class Remix(models.Model):
         # Extract colors from square_cover
         if self.square_cover:
             # Save the image temporarily to access it
-            temp_path = default_storage.save(
-                self.square_cover.name, ContentFile(self.square_cover.read()))
+            temp_path = default_storage.save(self.square_cover.name, ContentFile(self.square_cover.read()))
             temp_file_path = default_storage.path(temp_path)
 
             try:
@@ -104,8 +99,7 @@ class Remix(models.Model):
                 self.color = hex_color
                 self.dark_color = dark_hex
                 self.light_color = light_hex
-                super(Remix, self).save(update_fields=[
-                    'color', 'dark_color', 'light_color'])
+                super(Remix, self).save(update_fields=['color', 'dark_color', 'light_color'])
 
             finally:
                 # Clean up temporary file
