697655cb8aad9e47613f629f1129afeaad34625d
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-12-30 17:04:24 +0300

    Second commit

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Customer, Purchase, MixHistory, PlaylistHistory, RemixHistory
+admin.site.register(Customer)
+admin.site.register(Purchase)
+admin.site.register(MixHistory)
+admin.site.register(RemixHistory)
+admin.site.register(PlaylistHistory)
