5b01a1874da663ac210324a4a729372a9ae16d20
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-01-03 19:50:49 +0300

    Added CustomerEditForm

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Customer
 from django.contrib.auth.models import User
 from django_recaptcha.fields import ReCaptchaField
 from django.contrib.auth.forms import UserCreationForm
@@ -37,3 +38,53 @@ class CustomUserCreationForm(UserCreationForm):
         email = cleaned_data.get("email")
         password = cleaned_data.get("password")
         return cleaned_data
+
+
+class CustomerEditForm(forms.ModelForm):
+    full_name = forms.CharField(
+        max_length=150,
+        required=True,
+        label='Full Name',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        required=True,
+        label='Email Address',
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    bio = forms.CharField(
+        max_length=200,
+        required=False,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 3,
+            'placeholder': 'Tell us about yourself...'
+        })
+    )
+    profile_picture = forms.ImageField(
+        required=False,
+        label='Profile Picture',
+        widget=forms.FileInput(attrs={'class': 'form-control-file'})
+    )
+
+    class Meta:
+        model = Customer
+        fields = ['bio', 'profile_picture']
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields['full_name'].initial = user.get_full_name()
+            self.fields['email'].initial = user.email
+
+    def save(self, commit=True):
+        customer = super().save(commit=False)
+        user = self.instance.user
+        user.first_name, user.last_name = self.cleaned_data['full_name'].split(
+            ' ', 1)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+            customer.save()
+        return customer
