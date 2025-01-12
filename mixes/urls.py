a0825adb368557d5449ae17c07e4b225b69e94ec
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-01-12 19:15:34 +0300

    Added download urls

--- mixes/urls.py
+++ mixes/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from .utils import like_mix, reshare_mix
-from .views import mix_list, filtered_genres, filtered_albums, mix_detail, support_mix
+from .utils import update_download_count
+from .views import mix_list, filtered_genres, filtered_albums, mix_detail, support_mix, download_mix
 
 urlpatterns = [
     path("mixes/", mix_list, name="mix_list"),
     path("support/mix/", support_mix, name="support_mix"),
     path("mix/<slug:slug>/", mix_detail, name="mix_detail"),
+    path("mix/download/<slug:slug>/", download_mix, name="download_mix"),
     path("mixes/genre/<slug:slug>/", filtered_genres, name="filtered_genres"),
     path("mixes/albums/<slug:slug>/", filtered_albums, name="filtered_albums"),
 
     # utils
-    path("mixes/like-mix/<slug:slug>/", like_mix, name="like_mix"),
-    path("mixes/reshare-mix/<slug:slug>/", reshare_mix, name="reshare_mix"),
+    path('download/update_download/', update_download_count, name='update_download_count'),
 ]
